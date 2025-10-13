deedfbdc7d7c9ca8ca7284a414e36459031d39d6
ketiboldiais <numerstack@gmail.com>
2025-10-12 22:03:34 -0400

    update civil procedure outline

--- src/app/links.tsx
+++ src/app/links.tsx
@@ -1,7 +1,9 @@
 export const law_links = [
     { title: "Civil Procedure", url: "/pages/civil-procedure" },
     { title: "Constitutional Law", url: "/pages/constitutional-law" },
+    { title: "Immigration Law", url: "/pages/immigration-law" },
     { title: "U.S. Legal History", url: "/pages/us-legal-history" },
+    { title: "Military Law", url: "/pages/military-law" },
     { title: "Legal Research & Writing", url: "/pages/lrw" },
 ];
 
